6067b990925d33b6b389180630900108f0c1092f
naveen252006 <naveenkalaiarasan2006@gmail.com>
2026-01-19 12:59:28 +0530

    Added search feature

---
+++ search.py
@@ -0,0 +1,9 @@
+rooms = ["Deluxe", "Suite", "Standard", "Executive"]
+def search_room(query):
+    results = [room for room in rooms if query.lower() in room.lower()]
+    if results:
+        print("Available rooms:", results)
+    else:
+        print("No rooms found.")
+if __name__ == "__main__":
+    search_room("Suite")
